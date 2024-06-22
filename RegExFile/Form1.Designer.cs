2b72ccc69f4e9e41fb4b54a82445a0d0fb873c10
dido1092 <deyangospodinov1092@abv.bg>
2024-06-22 11:37:32 +0300

    Add project files.

---
+++ RegExFile/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace RegExFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxRegEx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBoxFileWindow = new System.Windows.Forms.RichTextBox();
+            this.buttonChoiceFile = new System.Windows.Forms.Button();
+            this.buttonPrevious = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.labelPages = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.labelDestination = new System.Windows.Forms.Label();
+            this.textBoxPage = new System.Windows.Forms.TextBox();
+            this.buttonSelectPege = new System.Windows.Forms.Button();
+            this.comboBoxExtension = new System.Windows.Forms.ComboBox();
+            this.hScrollBarFont = new System.Windows.Forms.HScrollBar();
+            this.SuspendLayout();
+            // 
+            // textBoxRegEx
+            // 
+            this.textBoxRegEx.Location = new System.Drawing.Point(178, 25);
+            this.textBoxRegEx.Name = "textBoxRegEx";
+            this.textBoxRegEx.Size = new System.Drawing.Size(642, 20);
+            this.textBoxRegEx.TabIndex = 0;
+            this.textBoxRegEx.TextChanged += new System.EventHandler(this.textBoxRegEx_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Regular Expression (Mark Unique Symbols)";
+            // 
+            // richTextBoxFileWindow
+            // 
+            this.richTextBoxFileWindow.Location = new System.Drawing.Point(45, 123);
+            this.richTextBoxFileWindow.Name = "richTextBoxFileWindow";
+            this.richTextBoxFileWindow.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBoxFileWindow.Size = new System.Drawing.Size(775, 570);
+            this.richTextBoxFileWindow.TabIndex = 2;
+            this.richTextBoxFileWindow.Text = "";
+            this.richTextBoxFileWindow.TextChanged += new System.EventHandler(this.richTextBoxFileWindow_TextChanged);
+            // 
+            // buttonChoiceFile
+            // 
+            this.buttonChoiceFile.Location = new System.Drawing.Point(45, 16);
+            this.buttonChoiceFile.Name = "buttonChoiceFile";
+            this.buttonChoiceFile.Size = new System.Drawing.Size(105, 36);
+            this.buttonChoiceFile.TabIndex = 3;
+            this.buttonChoiceFile.Text = "Choice File";
+            this.buttonChoiceFile.UseVisualStyleBackColor = true;
+            this.buttonChoiceFile.Click += new System.EventHandler(this.buttonChoiceFile_Click);
+            // 
+            // buttonPrevious
+            // 
+            this.buttonPrevious.Location = new System.Drawing.Point(45, 94);
+            this.buttonPrevious.Name = "buttonPrevious";
+            this.buttonPrevious.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrevious.TabIndex = 4;
+            this.buttonPrevious.Text = "<<";
+            this.buttonPrevious.UseVisualStyleBackColor = true;
+            this.buttonPrevious.Click += new System.EventHandler(this.buttonPrevious_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(745, 94);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 5;
+            this.buttonNext.Text = ">>";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // labelPages
+            // 
+            this.labelPages.AutoSize = true;
+            this.labelPages.Location = new System.Drawing.Point(352, 104);
+            this.labelPages.Name = "labelPages";
+            this.labelPages.Size = new System.Drawing.Size(24, 13);
+            this.labelPages.TabIndex = 6;
+            this.labelPages.Text = "0/0";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(858, 25);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(120, 46);
+            this.buttonStart.TabIndex = 7;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(826, 159);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(93, 29);
+            this.buttonSave.TabIndex = 8;
+            this.buttonSave.Text = "S A V E";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // labelDestination
+            // 
+            this.labelDestination.AutoSize = true;
+            this.labelDestination.Location = new System.Drawing.Point(42, 706);
+            this.labelDestination.Name = "labelDestination";
+            this.labelDestination.Size = new System.Drawing.Size(63, 13);
+            this.labelDestination.TabIndex = 9;
+            this.labelDestination.Text = "Destination:";
+            // 
+            // textBoxPage
+            // 
+            this.textBoxPage.Location = new System.Drawing.Point(346, 67);
+            this.textBoxPage.Name = "textBoxPage";
+            this.textBoxPage.Size = new System.Drawing.Size(48, 20);
+            this.textBoxPage.TabIndex = 10;
+            // 
+            // buttonSelectPege
+            // 
+            this.buttonSelectPege.Location = new System.Drawing.Point(400, 62);
+            this.buttonSelectPege.Name = "buttonSelectPege";
+            this.buttonSelectPege.Size = new System.Drawing.Size(75, 28);
+            this.buttonSelectPege.TabIndex = 11;
+            this.buttonSelectPege.Text = "Select Page";
+            this.buttonSelectPege.UseVisualStyleBackColor = true;
+            this.buttonSelectPege.Click += new System.EventHandler(this.buttonSelectPege_Click);
+            // 
+            // comboBoxExtension
+            // 
+            this.comboBoxExtension.FormattingEnabled = true;
+            this.comboBoxExtension.Items.AddRange(new object[] {
+            "TXT",
+            "PDF",
+            "DOCX"});
+            this.comboBoxExtension.Location = new System.Drawing.Point(826, 123);
+            this.comboBoxExtension.Name = "comboBoxExtension";
+            this.comboBoxExtension.Size = new System.Drawing.Size(93, 21);
+            this.comboBoxExtension.TabIndex = 12;
+            // 
+            // hScrollBarFont
+            // 
+            this.hScrollBarFont.Location = new System.Drawing.Point(565, 73);
+            this.hScrollBarFont.Name = "hScrollBarFont";
+            this.hScrollBarFont.Size = new System.Drawing.Size(80, 17);
+            this.hScrollBarFont.TabIndex = 13;
+            this.hScrollBarFont.Value = 5;
+            this.hScrollBarFont.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBarFont_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 755);
+            this.Controls.Add(this.hScrollBarFont);
+            this.Controls.Add(this.comboBoxExtension);
+            this.Controls.Add(this.buttonSelectPege);
+            this.Controls.Add(this.textBoxPage);
+            this.Controls.Add(this.labelDestination);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelPages);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonPrevious);
+            this.Controls.Add(this.buttonChoiceFile);
+            this.Controls.Add(this.richTextBoxFileWindow);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxRegEx);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RegExFile";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxRegEx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBoxFileWindow;
+        private System.Windows.Forms.Button buttonChoiceFile;
+        private System.Windows.Forms.Button buttonPrevious;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Label labelPages;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label labelDestination;
+        private System.Windows.Forms.TextBox textBoxPage;
+        private System.Windows.Forms.Button buttonSelectPege;
+        private System.Windows.Forms.ComboBox comboBoxExtension;
+        private System.Windows.Forms.HScrollBar hScrollBarFont;
+    }
+}
+
---
+++ RegExFile/Form1.cs
@@ -0,0 +1,269 @@
+﻿using iTextSharp.text.pdf.parser;
+using iTextSharp.text.pdf;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using Org.BouncyCastle.Asn1.Crmf;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Drawing;
+using System.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.AxHost;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using static iTextSharp.text.pdf.PdfDocument;
+using iTextSharp.text;
+using System.Runtime.InteropServices.ComTypes;
+using pdftron.PDF;
+using SixLabors.Fonts.Unicode;
+using pdftron.SDF;
+using System.Xml.Linq;
+using System.Runtime.Remoting.Messaging;
+using SautinSoft.Document;
+using System.Linq;
+using System.Windows.Documents;
+using System.Windows.Controls;
+
+namespace RegExFile
+{
+    public partial class Form1 : Form
+    {
+        private static string pathToAddFiles = string.Empty;
+        private static string[] pathWithFile = { };
+        private static int page = 1;
+        private static int numPages = 0;
+        private static string text = string.Empty;
+        private static string name = string.Empty;
+        private static string destination = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void textBoxRegEx_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.textBoxRegEx.TextAlignChanged += textBoxRegEx_TextChanged;
+
+                richTextBoxFileWindow.Text = textBoxRegEx.Text;
+            }
+        }
+        private void textBoxRegEx_TextChanged(object sender, EventArgs e)
+        {
+            //textBoxRegEx_KeyDown(textBoxRegEx, new KeyEventArgs(Keys.Enter));
+        }
+
+        private void buttonChoiceFile_Click(object sender, EventArgs e)
+        {
+            GetPath();
+            ReadPdf(pathWithFile[0]);
+        }
+        public void ReadPdf(string filePath)
+        {
+            PdfReader reader = new PdfReader(filePath);
+
+            numPages = reader.NumberOfPages;
+
+            ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+            string pageContent = PdfTextExtractor.GetTextFromPage(reader, page, strategy);
+
+            richTextBoxFileWindow.Text = ($"\n{pageContent}");
+            labelPages.Text = $"{page}/{numPages}";
+            text = pageContent;
+
+            reader.Close();
+        }
+        private string[] GetPath()
+        {
+            OpenFileDialog openFileDialog1 = OpenFile();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pathToAddFiles = System.IO.Path.GetDirectoryName(openFileDialog1.FileName);
+
+                pathWithFile = openFileDialog1.FileNames;
+            }
+
+            return pathWithFile;
+        }
+        private static OpenFileDialog OpenFile()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog()
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse AudioVideo Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "All Files",
+                Filter = "All Files (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                Multiselect = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true,
+            };
+            return openFileDialog1;
+        }
+
+        private void buttonPrevious_Click(object sender, EventArgs e)
+        {
+            if (page > 1)
+            {
+                page--;
+                ReadPdf(pathWithFile[0]);
+            }
+            else
+            {
+                page = 1;
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (page < numPages)
+            {
+                page++;
+                ReadPdf(pathWithFile[0]);
+            }
+            else
+            {
+                page = numPages;
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            string[] lines = text.Split(' ', '\n', '\t', '.', ',');
+
+            string pattern = $"{textBoxRegEx.Text}";//.Replace(@"\s", "").Replace(@"\t", "").Replace(@"\n", "");
+
+            foreach (var currentLine in lines)
+            {
+                Match match = Regex.Match(currentLine, pattern);
+
+                while (match.Success)
+                {
+                    if (currentLine != "")
+                    {
+                        name = match.Groups[1].Value.ToLower();
+
+                        int index = text.IndexOf(name);
+
+                        if (index != 0 && index != -1)
+                        {
+                            System.Drawing.Font fnt = new System.Drawing.Font("Verdana", 8F, FontStyle.Bold, GraphicsUnit.Point);
+
+                            richTextBoxFileWindow.Select(index + 1, name.Length);
+
+                            richTextBoxFileWindow.SelectionFont = fnt;
+                            richTextBoxFileWindow.SelectionColor = System.Drawing.Color.CadetBlue;
+                            //richTextBoxFileWindow.SelectionColor = Color.Green;
+                        }
+                        match = match.NextMatch();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveDestination();
+
+            labelDestination.Text = $"Destination: {destination}";
+        }
+        private void SaveDestination()
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+
+            string createText = richTextBoxFileWindow.Text;
+            string extension = comboBoxExtension.Text.ToLower();
+            if (extension == "" || extension == " ")
+            {
+                MessageBox.Show("Select extension!");
+            }
+            //else if (extension == "txt")
+            //{
+            //    if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            //    {
+            //        destination = folderBrowserDialog1.SelectedPath + $"\\{page}.{extension}";
+            //    }
+            //    else
+            //    {
+            //        destination = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + $"\\{page}.{extension}";
+            //    }
+            //    File.WriteAllText(destination, createText);
+            //}
+            else// if (extension == "pdf")
+            {
+
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    destination = folderBrowserDialog1.SelectedPath + $"\\{page}.{extension}";
+                }
+                else
+                {
+                    destination = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + $"\\{page}.{extension}";
+                }
+
+                DocumentCore dc = new DocumentCore();
+                ContentRange cr = dc.Content;
+
+                cr.Start.Insert(createText);
+                dc.Save(destination);
+
+                //cr = dc.Content.Find("SautinSoft.Document 2024.4.24 trial. Get your free 30-day key instantly: https://sautinsoft.com/start-for-free/").FirstOrDefault();
+                //cr.Start.Insert("");
+                //destination = folderBrowserDialog1.SelectedPath + $"\\Without logo {page}.{extension}";
+                //dc.Save(destination);
+
+                MessageBox.Show("Saved!");
+            }
+        }
+
+        private void buttonSelectPege_Click(object sender, EventArgs e)
+        {
+            bool isNumber = false;
+
+            isNumber = int.TryParse(textBoxPage.Text, out page);
+            if (isNumber)
+            {
+                page = int.Parse(textBoxPage.Text);
+                ReadPdf(pathWithFile[0]);
+            }
+        }
+
+        private System.Windows.Forms.RichTextBox GetRichTextBoxFileWindow()
+        {
+            return richTextBoxFileWindow;
+        }
+
+        private void richTextBoxFileWindow_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hScrollBarFont_Scroll(object sender, ScrollEventArgs e)
+        {
+            // Get the current scrollbar value (e.g., between 6 and maximum)
+            if (hScrollBarFont.Value > 6)
+            {
+                // Define your logic to calculate the new font size based on scrollValue
+                float newFontSize = hScrollBarFont.Value;
+
+                // Apply the new font size (entire content or selected text)
+                richTextBoxFileWindow.Font = new System.Drawing.Font(richTextBoxFileWindow.Font.FontFamily, newFontSize);
+            }
+        }
+    }
+}
